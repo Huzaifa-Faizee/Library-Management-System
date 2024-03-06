5cdcb8419e1bfd94cc164d57e08ce7eb7c85bab4
Huzaifa-Faizee <96124697+Huzaifa-Faizee@users.noreply.github.com>
2024-03-06 21:15:16 +0530

    Updates Login and signup

--- application/controllers.py
+++ application/controllers.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, request
 from flask import current_app as app
 from .models import *
-import datetime
+# import datetime
 
 
 @app.route('/')
@@ -14,12 +14,15 @@ def signIn():
     if request.method == "GET":
         return render_template("signin.html")
     elif request.method == "POST":
-        u_name = request.form.get("u_name")
+        u_email = request.form.get("u_email")
         pwd = request.form.get("pwd")
-        this_user = User.query.filter_by(user_name=u_name).first()
+        this_user = User.query.filter_by(user_email=u_email).first()
         if this_user:
             if this_user.password == pwd:
-                return "Logged In"
+                if(this_user.user_type=="admin"):
+                    return render_template("admin/admin_dashboard.html")
+                else:
+                    return render_template("user_dashboard.html")
             else:
                 return "Wrong Password"
 
@@ -40,4 +43,4 @@ def signUp():
             new_user = User(user_name=u_name, user_email=u_email, password=pwd)
             db.session.add(new_user)
             db.session.commit()
-            return "Signed In!"
+            return render_template("signin.html")
